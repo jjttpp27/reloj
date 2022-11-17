90bf678649b20a6cdf4938745df4417c0bfe1416
jjttpp27 <jjttpp27@gmail.com>
2022-11-17 14:47:56 +0100

    PrimerCommitPortafolio

--- reloj.js
+++ reloj.js
@@ -1,9 +1,12 @@
 
-//VARIABLES QUE NECESITAMOS
+//PRIMERO VAMOS A DEFINIR LAS VARIABLES QUE VAMOS A NECESITAMOS
 var currentTime = new Date();
 var year = currentTime.getFullYear();
 var month = currentTime.getMonth();
 var day = currentTime.getDay();
+
+/*En esta parte creamos un if en (horas, minutos, segundos)para que cuando el numero sea menor que 0 
+tenga un cero a la izquierda como en los relojes digitales y no sea solo un digito.*/
 
 var hour = currentTime.getHours();
 if (hour < 10) {
@@ -19,8 +22,11 @@ if (seconds < 10) {
 }
 
 //EJERCICIO 3 ---->  PARTE DEL RELOJ
+//Procedemos a crear el reloj con la funcion setInterval que he puesto que se refresque cada 1 segundo
+
 setInterval(() => {
-    var reloj = document.querySelector(".reloj");
+    //Usamos el querySelector para indicar donde quiero enseñar la informacion 
+    var reloj = document.querySelector("#reloj");
     var currentTime = new Date();
 
     var hour = currentTime.getHours();
@@ -35,6 +41,7 @@ setInterval(() => {
     if (seconds < 10) {
         seconds = `0${seconds}`
     }
+    //Finalmente enseñamos el resultado que queremos
     reloj.innerHTML = `${hour}:${minutes}:${seconds}`;
 
 }, 1000);
@@ -42,7 +49,8 @@ setInterval(() => {
 
 
 //EJERCICIO 1 PARTE DEL SALUDO
-var saludo = document.querySelector(".saludo");
+//Para el saludo he seleccionado un "div", donde ira el saludo y he procedido a crear una condicional.
+var saludo = document.querySelector("#saludo");
 
 if (hour >= 6 && hour < 12) {
     saludo.innerHTML = "Buenos días";
@@ -55,6 +63,7 @@ else if (hour > 12 && hour < 20) {
 //TERMINA PARTE DEL SALUDO
 
 //EJERCICIO 2 PARTE DEL STRING EN FECHA
+//En este ejercicio tube que crear un array con los nombres de los dias.
 const dias = [
     'domingo',
     'lunes',
@@ -64,12 +73,14 @@ const dias = [
     'viernes',
     'sábado',
 ];
+//Con esta variable seleccionara el dia y lo convertira al nombre en String
 var dayName = dias[day]
 
-let mesName = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(new Date());
+//Consegui un metodo q te devolvia el nombre del mes, sino hubiese hecho otro array con los meses.
+var mesName = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(new Date());
 
-
-var fecha = document.querySelector(".fecha");
+//Finalmente el resultado final con la frase terminada.
+var fecha = document.querySelector("#fecha");
 fecha.innerHTML = `Hoy es ${dayName}, ${day} de ${mesName} de ${year} y son las ${hour}:${minutes}`
 
 //TERMINA PARTE DE FECHA
